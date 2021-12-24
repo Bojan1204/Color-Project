66d877715279b337d45e467796119db4fecf8aed
Bojan Bosnic <56975621+Bojan1204@users.noreply.github.com>
2021-12-24 18:13:37 +0100

    react router was added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.scss';
+import{ Route, Switch } from "react-router-dom";
 import Palette from './components/Palette';
 import seedColors from './components/seedColors';
 import { generatePalette } from "./components/colorHelpers";
 
 
 
-
 class  App extends React.Component {
   render(){
-    console.log(generatePalette(seedColors[4]))
+    
     return (
-      <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>  
+      <Route exact path='/' render={()=> <h1>Palette list foe here</h1>} />
+      <Route path='/palette/:id'render={()=> <p>druga lista</p>}/>
+      </Switch>
+      // <div className="App">
+      // <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   } 
  
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
-  <BrowserRouter> 
+<BrowserRouter> 
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>,
+  </BrowserRouter>
+ ,
   document.getElementById('root')
 );
 
